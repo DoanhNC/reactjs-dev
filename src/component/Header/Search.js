53481c3b29be544fd5b178c63be5726f4d918151
Doanh <congdoanh95@gmail.com>
2018-06-28 17:04:24 +0700

    doanh ..

--- src/component/Header/Search.js
+++ src/component/Header/Search.js
@@ -1,15 +1,35 @@
 import React, { Component }
 from 'react';
 class Search extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            'strSearch': '123456'
+        };
+        this.btnSearch = this.btnSearch.bind(this);
+        this.btnClear = this.btnClear.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    btnSearch(){
+        console.log('search', this.state.strSearch);  
+    };
+    btnClear(){
+        this.setState({strSearch: ''});
+    }
+    handleChange(event){
+      this.setState({strSearch: event.target.value});
+    };
     render() {
         return (
                 <div>
                     <form className="navbar-form navbar-right" action="/action_page.php">
                         <div className="col-lg-12">
                             <div className="input-group">
-                                <input type="text" className="form-control" placeholder="Thông tin tìm kiếm..." />
+                                <input type="text" className="form-control" value={this.state.strSearch} onChange={this.handleChange} placeholder="Thông tin tìm kiếm..." />
                                 <span className="input-group-btn">
-                                    <button className="btn btn-default" type="button">Tìm kiếm</button>
+                                    <button className="btn btn-default" type="button" onClick={this.btnSearch}>Tìm kiếm</button>
+                                    <button className="btn btn-warning" type="button" onClick={this.btnClear}>Xóa</button>
                                 </span>
                             </div>
                         </div>
